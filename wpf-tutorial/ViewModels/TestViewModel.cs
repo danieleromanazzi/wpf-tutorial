5b2885b0a9badcaa4137c9fd28760d8cf1cfc407
Daniele Romanazzi <daniele.romanazzi@live.com>
2019-11-24 10:25:10 +0100

    commit

---
+++ wpf-tutorial/ViewModels/TestViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_tutorial.ViewModels
+{
+    public class TestViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string myVar;
+
+        public string MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+
+    }
+}
