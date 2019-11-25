5ca80716532a82d860d3cb0033957b1ec35d9eeb
Romanazzi, Daniele <daniele.romanazzi@cgm.com>
2019-11-25 08:49:03 +0100

    varie for challenges prj

---
+++ varie/BaseViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace cgm.AIS.Windows.Controls.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        Dictionary<string, object> properties = new Dictionary<string, object>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string propertyName, object value = null)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                if (value == null)
+                    handler(this, new PropertyChangedEventArgs(propertyName));
+                else
+                    handler(value, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetValue<T>(string propertyName, T value)
+        {
+            if (!properties.ContainsKey(propertyName))
+            {
+                properties.Add(propertyName, default(T));
+            }
+
+            var oldValue = GetValue<T>(propertyName);
+            if (!EqualityComparer<T>.Default.Equals(oldValue, value))
+            {
+                properties[propertyName] = value;
+                OnPropertyChanged(propertyName);
+            }
+        }
+
+        protected T GetValue<T>(string propertyName)
+        {
+            if (!properties.ContainsKey(propertyName))
+            {
+                return default(T);
+            }
+            else
+            {
+                return (T)properties[propertyName];
+            }
+        }
+
+    }
+}
---
+++ varie/WindowVisibilityTimeout.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace cgm.AIS.Windows.Controls.Behaviors
+{
+    public class WindowVisibilityTimeout
+    {
+        public static readonly DependencyProperty VisibilityTimeoutProperty =
+            DependencyProperty.RegisterAttached("VisibilityTimeout", typeof(int), typeof(WindowVisibilityTimeout), new PropertyMetadata(OnVisibilityTimeoutChanged));
+
+        public static int GetVisibilityTimeout(DependencyObject dependencyObject)
+        {
+            return (int)dependencyObject.GetValue(VisibilityTimeoutProperty);
+        }
+
+        public static void SetVisibilityTimeout(DependencyObject dependencyObject, int navigate)
+        {
+            dependencyObject.SetValue(VisibilityTimeoutProperty, navigate);
+        }
+
+        private static void OnVisibilityTimeoutChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Window window = d as Window;
+            if (window != null)
+            {
+                window.Visibility = System.Windows.Visibility.Visible;
+                int value = (int)e.NewValue;
+
+                Common.DispatcherTimeout.Timeout(
+                    DispatcherPriority.Normal,
+                    TimeSpan.FromSeconds(2.0),
+                    timeout =>
+                    {
+                        window.Visibility = System.Windows.Visibility.Collapsed;
+                        SetVisibilityTimeout(d, 0);
+                    });
+            }
+        }
+    }
+}
