8191ba4a617bdfe936fd7fdd98ed9db7c11eb096
Daniele Romanazzi <daniele.romanazzi@live.com>
2019-12-04 22:20:57 +0100

    resources

--- wpf-challenge-resources/MainWindow.xaml.cs
+++ wpf-challenge-resources/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace wpf_challenge_simplecontrols
+namespace wpf_challenge_resources
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,11 +23,6 @@ namespace wpf_challenge_simplecontrols
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Login.Visibility = Visibility.Collapsed;
         }
     }
 }
